59f296ccf07ef8af27cfe174be9fc167e76ff0ee
Yannick Simard <ysimard@gmail.com>
2013-10-19 10:20:10 -0400

    uses foreach

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ var http = require('http'),
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/json'});
-  
+
   exec("df -lh", function(error, stdout, stderr) {
 
    	var lines = stdout.trim().split('\n'),
         header = lines.shift().split(/[\s]+/),
         drives = [];
 
-    for(var line = 0; line < lines.length; line++) {
-      var drive = {},
-          properties = lines[line].split(/[\s]+/);
 
-      for(var prop = 0; prop<header.length; prop++) {
-        drive[header[prop].toLowerCase()] = properties[prop];
-      }
+    lines.forEach(function(line, index, array) {
+      var drive = {},
+          properties = line.split(/[\s]+/);
+
+      properties.forEach(function(property, index, array) {
+        drive[header[index].toLowerCase()] = property; 
+      });
+
       drives.push(drive);
-    }
+    });
+
 
    	res.end(JSON.stringify(drives));
    });
